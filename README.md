0674c8956a23623f4b9c7142cbb95acc7c32e0be
Podtopa <martsonnnn@gmail.com>
2021-11-18 21:43:13 +0200

    Init CV

---
+++ README.md
@@ -0,0 +1,22 @@
+# Anastasiia Podtopa CV
+
+### Quickstart
+
+#### 1. Install dependencies
+```sh
+npm i
+```
+
+#### 2. Build the app
+
+```sh
+nom run build
+```
+
+#### 3. Start the app
+
+```sh
+nom run start
+```
+
+[Anastasiia Podtopa](https://github.com/Podtopa)
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const miniCss = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'assets/[name].[ext]',
+                        },
+                    }
+                ],
+            },
+            {
+                test:/\.(s*)css$/,
+                use: [
+                    miniCss.loader,
+                    'css-loader',
+                    'sass-loader',
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new miniCss({
+            filename: 'style.css',
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+        }),
+    ]
+};
